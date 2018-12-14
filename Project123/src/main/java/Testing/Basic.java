3578778f84e305f52047a64621e0a90048a75359
Dhanaraj <Dhanarajvlsidesign@gmail.com>
2018-12-14 08:05:54 +0530

    modification in the Basic.java program

--- Project123/src/main/java/Testing/Basic.java
+++ Project123/src/main/java/Testing/Basic.java
@@ -2,12 +2,21 @@ package Testing;
 
 public class Basic {
 	
+	
+	public void add()
+	{
+		System.out.println("The value of this number");
+	}
+	
 	public static void main(String[] args) {
 		
 		int a=10,b=25,c;
 		
 		 c=a+b;
 		System.out.println(c);
+		
+		
+		
 	}
 
 }
